59081d538c691df4a8c3e7418d11b33c3d2db86b
Vars3 <NirSegal@gmail.com>
2017-12-30 01:44:33 +0200

    Update py_test_file.py

--- py_test_file.py
+++ py_test_file.py
@@ -1,6 +1,12 @@
+import sys, os
+
+
+def foo(x, y):
+	return (x * (y**3 + 15)) - 130
+
+
 def main():
 	print 'Hello World!\n\tThis is a test file.'
-	print '\n\nPress Enter to close. . .'
 	raw_input()
 if __name__ == '__main__':
 	main()
